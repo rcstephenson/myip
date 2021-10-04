5a74ec4ee263cf8830305126c080f58a18e17701
Ryan Stephenson <ryanstephenson2013@gmail.com>
2021-10-04 14:27:18 -0700

    comp. syntax error

--- cordic/rtl/cordic.vhd
+++ cordic/rtl/cordic.vhd
@@ -1,23 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
---use ieee.std_logic_unsigned.all;
+
 
 
 entity cordic is
-  generic (PB       : integer := 16;  -- Phase Bit Width
-           DB       : integer := 16;  -- Data Bit Width
+  generic (PW       : integer := 16;  -- Phase Bit Width
+           XYW      : integer := 16;  -- Data Bit Width
            WW       : integer := 17;  -- Working Data Width
            N_Stages : integer := 4);  -- Number of Rotations
   port (
     clk         :   in      std_logic;
     arst_n      :   in      std_logic; 
-    i_phi       :   in      std_logic_vector(PB-1 downto 0); -- unsigned
-    i_xval      :   in      std_logic_vector(DB-1 downto 0); -- signed
-    i_yval      :   in      std_logic_vector(DB-1 downto 0); -- signed
-    o_phi       :   out     std_logic_vector(PB-1 downto 0); -- unsigned
-    o_xval      :   out     std_logic_vector(DB-1 downto 0); -- signed
-    o_yval      :   out     std_logic_vector(DB-1 downto 0); -- signed
+    i_phi       :   in      std_logic_vector(PW-1 downto 0); -- unsigned
+    i_xval      :   in      std_logic_vector(XYW-1 downto 0); -- signed
+    i_yval      :   in      std_logic_vector(XYW-1 downto 0); -- signed
+    o_phi       :   out     std_logic_vector(PW-1 downto 0); -- unsigned
+    o_xval      :   out     std_logic_vector(XYW-1 downto 0); -- signed
+    o_yval      :   out     std_logic_vector(XYW-1 downto 0) -- signed
   ) ;
 end cordic;
 
@@ -27,17 +28,17 @@ architecture rtl of cordic is
     CONSTANT PI         : unsigned(PW-1 downto 0) := to_unsigned(2**(PW-1)-1,PW);
     CONSTANT HALFPI     : unsigned(PW-1 downto 0) := to_unsigned(2**(PW-2)-1,PW);
     CONSTANT QUARTERPI  : unsigned(PW-1 downto 0) := to_unsigned(2**(PW-3)-1,PW);
-    type xy_arr_t is array(N_Stages to 0) of   signed(WW-1 downto 0);
-    type ph_arr_t is array(N_Stages to 0) of unsigned(PW-1 downto 0);
+    type xy_arr_t is array(0 to N_Stages-1) of   signed(WW-1 downto 0);
+    type ph_arr_t is array(0 to N_Stages-1) of unsigned(PW-1 downto 0);
 
 
     signal e_xval   :   signed(WW-1 downto 0); -- 'extended' values for digital gain
     signal e_yval   :   signed(WW-1 downto 0);
-    signal octet    :   std_logic_vector(3 downto 0);
-    signal xv       :   xy_arr_t := (others<=(others<='0'));
-    signal yv       :   xy_arr_t := (others<=(others<='0'));
-    signal ph       :   ph_arr_t := (others<=(others<='0'));
-    signal cordic_angles : ph_arr_t := (X"4b90",X"27ec",X"1444",X"a2c");
+    signal octet    :   std_logic_vector(2 downto 0);
+    signal xv       :   xy_arr_t; --<= (others=>(others=>'0'));
+    signal yv       :   xy_arr_t; --<= (others=>(others=>'0'));
+    signal ph       :   ph_arr_t; --<= (others=>(others=>'0'));
+    signal cordic_angles : ph_arr_t := (X"4b90",X"27ec",X"1444",X"0a2c");
 
 begin 
 
@@ -56,58 +57,58 @@ begin
 
             e_xval <= resize(signed(i_xval),WW); -- add cordic gain scaling  
             e_yval <= resize(signed(i_yval),WW);
-            octet <= phi_i(N-1 downto N-4);
+            octet <= i_phi(PW-1 downto PW-3);
 
             case octet is 
                 when "000"|"111" => -- 0..45, 315..360
                     -- No Change
-                    xv[0] <=    e_yval;      
-                    yv[0] <=    e_xval;  
-                    ph[0] <=    i_phi;
+                    xv(0) <=    e_yval;          
+                    yv(0) <=    e_xval;  
+                    ph(0) <=    unsigned(i_phi);
                 when "001" => -- 45..90
-                    xv[0] <=   -e_yval;
-                    yv[0] <=   e_xval;
-                    ph[0] <=    i_phi + (PI + HALFPI);
+                    xv(0) <=   -e_yval;
+                    yv(0) <=   e_xval;
+                    ph(0) <=    unsigned(i_phi) - HALFPI;
                 when "010" => -- 90..135
-                    xv[0] <=   -e_yval;
-                    yv[0] <=    e_xval;
-                    ph[0] <=    i_phi + (PI + HALFPI);
+                    xv(0) <=   -e_yval;
+                    yv(0) <=    e_xval;
+                    ph(0) <=    unsigned(i_phi) - HALFPI;
                 when "011" => -- 135..180
-                    xv[0] <=   -e_xval;
-                    yv[0] <=   -e_yval;
-                    ph[0] <=    i_phi + PI;
+                    xv(0) <=   -e_xval;
+                    yv(0) <=   -e_yval;
+                    ph(0) <=    unsigned(i_phi) + PI;
                 when "100" => -- 180..225
-                    xv[0] <=   -e_xval;
-                    yv[0] <=   -e_yval;
-                    ph[0] <=    i_phi + PI;
+                    xv(0) <=   -e_xval;
+                    yv(0) <=   -e_yval;
+                    ph(0) <=    unsigned(i_phi) + PI;
                 when "101" => -- 225..270
-                    xv[0] <=   e_yval;
-                    yv[0] <=    -e_xval;
-                    ph[0] <=    i_phi + HALFPI;
+                    xv(0) <=   e_yval;
+                    yv(0) <=    -e_xval;
+                    ph(0) <=    unsigned(i_phi) + HALFPI;
                 when "110" => -- 270..315
-                    xv[0] <=   e_yval;
-                    yv[0] <=   -e_xval;                
-                    ph[0] <=    i_phi + HALFPI;
+                    xv(0) <=   e_yval;
+                    yv(0) <=   -e_xval;                
+                    ph(0) <=    unsigned(i_phi) + HALFPI;
                 when others =>
                     NULL;
             end case;
 
             for i in 0 to (N_stages-1) loop
-                if (ph[i](PW-1) = 1) 
-                    xv[i+1] <= xv[i] + shift_right(yv[i],i+1);
-                    yv[i+1] <= yv[i] - shift_right(yv[i],i+1);
-                    ph[i+1] <= ph[i] + cordic_angle[i];
+                if (ph(i)(PW-1) = '1') then
+                    xv(i+1) <= xv(i) + shift_right(yv(i),i+1);
+                    yv(i+1) <= yv(i) - shift_right(yv(i),i+1);
+                    ph(i+1) <= ph(i) + cordic_angles(i);
                 else 
-                    xv[i+1] <= xv[i] - shift_right(yv[i],i+1);
-                    yv[i+1] <= yv[i] + shift_right(yv[i],i+1);
-                    ph[i+1] <= ph[i] - cordic_angle[i];
+                    xv(i+1) <= xv(i) - shift_right(yv(i),i+1);
+                    yv(i+1) <= yv(i) + shift_right(yv(i),i+1);
+                    ph(i+1) <= ph(i) - cordic_angles(i);
                 end if;
             end loop;
 
             -- truncating output data...
-            o_xval <= std_logic_vector(resize(xv[N_stages],DW));
-            o_yval <= std_logic_vector(resize(yv[N_stages],DW));
-            o_phi  <= std_logic_vector(ph[N_stages]);
+            o_xval <= std_logic_vector(resize(xv(N_stages),XYW));
+            o_yval <= std_logic_vector(resize(yv(N_stages),XYW));
+            o_phi  <= std_logic_vector(ph(N_stages));
 
         end if;
     end if;
--- cordic/sim/cordic_tb.vhd
+++ cordic/sim/cordic_tb.vhd
@@ -7,45 +7,52 @@ entity cordic_tb is
 end cordic_tb;
 
 architecture testbench of cordic_tb is 
+    -- testbench constants and generics
     CONSTANT clk_period     : time      := 10 ns;
 
-    CONSTANT PB_TB          : integer   := 16;   
-    CONSTANT DB_TB          : integer   := 16;
+    CONSTANT PW_TB          : integer   := 16;   
+    CONSTANT XYW_TB          : integer   := 16;
     CONSTANT WW_TB          : integer   := 17;
-    CONSTANT N_Stages_TB                := 4;
+    CONSTANT N_Stages_TB    : integer   := 4;
 
-    CONSTANT TWOPI      : unsigned(PW-1 downto 0) := to_unsigned(2**(PW-1)-1,PW);
-    CONSTANT PI         : unsigned(PW-1 downto 0) := to_unsigned(2**(PW-2)-1,PW);
-    CONSTANT HALFPI     : unsigned(PW-1 downto 0) := to_unsigned(2**(PW-3)-1,PW);
-    CONSTANT QUARTERPI  : unsigned(PW-1 downto 0) := to_unsigned(2**(PW-4)-1,PW);
+    CONSTANT TWOPI      : unsigned(PW_TB-1 downto 0) := to_unsigned(2**(PW_TB)-1,PW_TB);
+    CONSTANT PI         : unsigned(PW_TB-1 downto 0) := to_unsigned(2**(PW_TB-1)-1,PW_TB);
+    CONSTANT HALFPI     : unsigned(PW_TB-1 downto 0) := to_unsigned(2**(PW_TB-2)-1,PW_TB);
+    CONSTANT QUARTERPI  : unsigned(PW_TB-1 downto 0) := to_unsigned(2**(PW_TB-3)-1,PW_TB);
 
+    -- componet under test
     component cordic is
-        generic (PB       : integer := PB_TB;         -- Phase Bit Width
-                 DB       : integer := DB_TB;         -- Data Bit Width
+        generic (PW      : integer := PW_TB;         -- Phase Bit Width
+                 XYW       : integer := XYW_TB;         -- Data Bit Width
                  WW       : integer := WW_TB;         -- Working Data Width
-                 N_Stages : integer := N_Stages_TB);  -- Number of Rotations
+                 N_Stages : integer := N_Stages_TB  );  -- Number of Rotations
         port (
           clk         :   in      std_logic;
           arst_n      :   in      std_logic; 
-          i_phi       :   in      std_logic_vector(PB-1 downto 0); -- unsigned
-          i_xval      :   in      std_logic_vector(DB-1 downto 0); -- signed
-          i_yval      :   in      std_logic_vector(DB-1 downto 0); -- signed
-          o_phi       :   out     std_logic_vector(PB-1 downto 0); -- unsigned
-          o_xval      :   out     std_logic_vector(DB-1 downto 0); -- signed
-          o_yval      :   out     std_logic_vector(DB-1 downto 0); -- signed
+          i_phi       :   in      std_logic_vector(PW_TB-1 downto 0); -- unsigned
+          i_xval      :   in      std_logic_vector(XYW_TB-1 downto 0); -- signed
+          i_yval      :   in      std_logic_vector(XYW_TB-1 downto 0); -- signed
+          o_phi       :   out     std_logic_vector(PW_TB-1 downto 0); -- unsigned
+          o_xval      :   out     std_logic_vector(XYW_TB-1 downto 0); -- signed
+          o_yval      :   out     std_logic_vector(XYW_TB-1 downto 0) -- signed
         ) ;
       end component;
+      -- io signals
       signal clk    : std_logic;
       signal arst_n : std_logic;
-      signal i_phi  : std_logic_vector( downto 0);
-      signal i_xval : std_logic_vector( downto 0);
-      signal i_yval : std_logic_vector( downto 0);
-      signal o_phi  : std_logic_vector( downto 0);
-      signal o_xval : std_logic_vector( downto 0);
-      signal o_yval : std_logic_vector( downto 0);
+      signal i_phi  : std_logic_vector(PW_TB-1 downto 0);
+      signal i_xval : std_logic_vector(XYW_TB-1 downto 0);
+      signal i_yval : std_logic_vector(XYW_TB-1 downto 0);
+      signal o_phi  : std_logic_vector(PW_TB-1 downto 0);
+      signal o_xval : std_logic_vector(XYW_TB-1 downto 0);
+      signal o_yval : std_logic_vector(XYW_TB-1 downto 0);
 begin 
 
         DUT : cordic 
+        -- generic map (
+        -- PW       => PW_TB,
+        -- XYW      => XYW_TB
+        -- )
         port map(
             clk    => clk   ,
             arst_n => arst_n,
@@ -76,7 +83,7 @@ begin
             i_xval <=  (others=>'1');
             i_yval <=  (others=>'0');  
 
-            for i in 0 to 2**(PW)-1 loop 
+            for i in 0 to 2**(PW_TB)-1 loop 
                 i_phi <= i_phi+1;
                 wait until falling_edge(clk);
             end loop;
